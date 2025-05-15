ae6b69d797c3842044aeedfb173e436f874f45fe
Yasir Saleem <yasir.saleem@devsinc.com>
2025-05-15 17:13:29 +0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,12 @@ builder.Services.AddDbContext<DataContext>( opt =>
 {opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-var app = builder.Build();
+builder.Services.AddCors();
 
+var app = builder.Build();
+ app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200" , "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
